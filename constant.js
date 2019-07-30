7fd8dba83c98125261b7e97b7e69d823a2edd827
duxianzhang@kaochong.com <787753295@qq.com>
2019-07-30 19:09:27 +0800

    feat: 第一版基本完成

---
+++ constant.js
@@ -0,0 +1,40 @@
+module.exports = {
+  url: 'http://jira.xuanke.com/browse/TEACHING-399?filter=-1',
+  cookies: [
+    {
+      name: 'UM_distinctid',
+      value: '16b1cee7c0e2a9-00427e00dd76a3-37637e03-13c680-16b1cee7c61460',
+      domain: '.xuanke.com'
+    },
+    {
+      name: 'kc_puid',
+      value: '6AED53F5ADA03143682428B5CAAFB3C2B85332DE0EAAA7ED488C0E4572142BD5724B1FA3EAAF4429DBE714BE62982AF7',
+      domain: '.xuanke.com'
+    },
+    {
+      name: 'seraph.rememberme.cookie',
+      value: '10498%3A603270cb789866c86c771720c887831ccd513060',
+      domain: 'jira.xuanke.com'
+    },
+    {
+      name: 'AJS.conglomerate.cookie',
+      value: '"|hipchat.inapp.links.first.clicked.duxianzhang=false|slack.inapp.links.first.clicked.duxianzhang=false"',
+      domain: 'jira.xuanke.com'
+    },
+    {
+      name: 'JSESSIONID',
+      value: '425FB286B55FE3F95A111338481CEB2E',
+      domain: 'jira.xuanke.com'
+    },
+    {
+      name: 'atlassian.xsrf.token',
+      value: 'B7JR-VEI6-1BD6-03U0|fc083dca97df435c61fe9f665b60930635b6879b|lin',
+      domain: 'jira.xuanke.com'
+    },
+    {
+      name: 'jira.editor.user.mode',
+      value: 'wysiwyg',
+      domain: 'jira.xuanke.com'
+    }
+  ]
+}
---
+++ index.js
@@ -0,0 +1,60 @@
+require('babel-polyfill');
+const puppeteer = require('puppeteer');
+
+// const config = require('./constant-behind')
+const config = require('./constant');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    devtools: false,
+    defaultViewport: {
+      width: 1000,
+      height: 700
+    },
+  })
+  console.log('opening page')
+  const page = await browser.newPage()
+  config.cookies && await page.setCookie(...config.cookies)
+  await page.goto(config.url, {
+    timeout:0,
+    waitUntil: 'networkidle2'
+  })
+  console.log('页面加载完毕')
+  
+  let length = 0
+  try {
+    // 获取issue-list长度
+    length = await page.$eval('.issue-list',(el) => 
+      Array.from(el.childNodes|| [])
+        .filter(
+          i => i.tagName.toUpperCase() === 'LI'
+        ).length)
+    
+    for (let i = 0; i < length; i++) {
+      await page.click(`.issue-list li:nth-child(${i + 1})`)
+      // console.log('点击item')
+      
+      await page.waitFor('#action_id_5')
+      await page.click('#action_id_5')
+      // console.log('点击解决问题')
+    
+      await page.waitFor('#resolution')
+      // console.log('点击下拉框')
+    
+      await page.select('#resolution', '10004')
+      // console.log('点击完成')
+    
+      await page.click('#issue-workflow-transition-submit')
+      // console.log('点击解决')
+
+      await page.waitFor(1000)
+      console.log(`【${i + 1}】 issue标记完成...`)
+    }
+    browser.close()
+  } catch (error) {
+    browser.close()
+    console.log('error');
+    console.log(error)
+  }
+})()
